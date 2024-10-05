f89b779926d1af4dd6e112aa74d313d59e4f8d13
fabian <fabian@haeger.info>
2024-10-05 21:03:08 +0200

    feat: Add variable name length and add godoc comments

--- generator.go
+++ generator.go
@@ -1,3 +1,5 @@
+// Package namegen implements a Name Generator that can be used to generate
+// random names for different things like servers, containers, connections etc.
 package namegen
 
 import (
@@ -7,24 +9,37 @@ import (
 
 type Generator interface {
 	Generate() string
+	GenerateLength(int) string
 }
 
 type NameGen struct {
 	random *rand.Rand
 }
 
-func (gen *NameGen) Generate() string {
-	firstAjective := adjectives[gen.random.Intn(len(adjectives))]
-	secondAdjective := adjectives[gen.random.Intn(len(adjectives))]
-	name := names[gen.random.Intn(len(names))]
-
-	return fmt.Sprintf("%s_%s_%s", firstAjective, secondAdjective, name)
-}
-
+// Create a new Generator object
 func NewNameGen(seed int64) Generator {
 	gen := &NameGen{
 		random: rand.New(rand.NewSource(seed)),
 	}
 
 	return gen
+}
+
+// Generate a random name with default length.
+// It is built out of two adjectives and one name.
+func (gen *NameGen) Generate() string {
+	return gen.GenerateLength(3)
+}
+
+// Generate a random name that ends with a name.
+// If length is greated than one, multiple adjectives are added before the name.
+func (gen *NameGen) GenerateLength(length int) string {
+	if length < 0 {
+		panic(fmt.Errorf("length must be greater than 0"))
+	}
+	ret := names[gen.random.Intn(len(names))]
+	for i := 1; i < length; i++ {
+		ret = fmt.Sprintf("%v_%v", adjectives[gen.random.Intn(len(adjectives))], ret)
+	}
+	return ret
 }
--- generator_test.go
+++ generator_test.go
@@ -23,3 +23,35 @@ func TestGeneate(t *testing.T) {
 		t.Errorf("Name doesn't match expected length")
 	}
 }
+
+func TestGenerateLength(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	nameGen := NewNameGen(seed)
+
+	onlyName := nameGen.GenerateLength(1)
+
+	defer func() {
+		for _, name := range names {
+			if onlyName == name {
+				return
+			}
+		}
+		t.Errorf("Name %s is not in names", onlyName)
+	}()
+
+	assertPanicNameGen(t, nameGen.GenerateLength)
+
+	longName := nameGen.GenerateLength(4)
+	if len(strings.Split(longName, "_")) != 4 {
+		t.Errorf("Length of generated name is wrong")
+	}
+}
+
+func assertPanicNameGen(t *testing.T, f func(int) string) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	f(-1)
+}
